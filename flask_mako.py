fdb61db5ad5ea1293c8b4b1e2186f05fd741987b
al3x5 <alexander.schoemaker@gmail.com>
2025-10-29 23:35:10 +0100

    Replace cached_property import from flask with werkzeug

--- flask_mako.py
+++ flask_mako.py
@@ -11,7 +11,7 @@
 """
 import os, sys
 
-from flask.helpers import cached_property
+from werkzeug.utils import cached_property
 from flask.signals import template_rendered
 
 # Find the context stack so we can resolve which application is calling this
